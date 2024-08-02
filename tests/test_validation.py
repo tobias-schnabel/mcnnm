9a4bbc743285a3a74357f19d7db0daad87d808a6
Tobias Schnabel <ctobiasschnabel@gmail.com>
2024-08-02 16:12:43 +0100

    merge CV and holdout validation tests

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -18,8 +18,8 @@ jax.config.update("jax_enable_x64", True)
 # jax.config.update("jax_disable_jit", True)
 
 
-@pytest.mark.timeout(300)
-def test_mcnnm_accuracy_no_covariates(tolerance=0.1):
+@pytest.mark.timeout(30)
+def test_mcnnm_accuracy_cv_no_covariates(tolerance=0.1):
     nobs, nperiods = 100, 100
     data, true_params = generate_data(
         nobs=nobs,
@@ -37,17 +37,10 @@ def test_mcnnm_accuracy_no_covariates(tolerance=0.1):
 
     results = estimate(Y, W, return_fixed_effects=True)
 
-    print(f"\nTrue effect: {true_params['treatment_effect']}, Estimated effect: {results.tau:.4f}")
     assert_close(
         true_params["treatment_effect"], results.tau, tolerance, "Estimated treatment effect"
     )
 
-    print(f"Chosen lambda_L: {results.lambda_L:.4f}")
-
-    print("\nFixed Effects Comparison:")
-    print("Unit Fixed Effects:")
-    print(f"True mean:      {jnp.mean(true_params['unit_fe']):.4f}")
-    print(f"Estimated mean: {jnp.mean(results.gamma):.4f}")
     assert_close(
         jnp.mean(true_params["unit_fe"]),
         jnp.mean(results.gamma),
@@ -55,9 +48,6 @@ def test_mcnnm_accuracy_no_covariates(tolerance=0.1):
         "Estimated unit fixed effects mean",
     )
 
-    print("\nTime Fixed Effects:")
-    print(f"True mean:      {jnp.mean(true_params['time_fe']):.4f}")
-    print(f"Estimated mean: {jnp.mean(results.delta):.4f}")
     assert_close(
         jnp.mean(true_params["time_fe"]),
         jnp.mean(results.delta),
@@ -65,13 +55,11 @@ def test_mcnnm_accuracy_no_covariates(tolerance=0.1):
         "Estimated time fixed effects mean",
     )
 
-    print(f"\nTrue L mean: {jnp.mean(true_params['L']):.4f}")
-    print(f"Estimated L mean: {jnp.mean(results.L):.4f}")
     assert_close(jnp.mean(true_params["L"]), jnp.mean(results.L), tolerance, "Estimated L mean")
 
 
-@pytest.mark.timeout(600)
-def test_mcnnm_accuracy(tolerance=0.2):
+@pytest.mark.timeout(30)
+def test_mcnnm_accuracy_cv(tolerance=0.2):
     nobs, nperiods = 100, 100
     data, true_params = generate_data(nobs=nobs, nperiods=nperiods, seed=42)
 
@@ -84,18 +72,10 @@ def test_mcnnm_accuracy(tolerance=0.2):
         Y, W, X=X, Z=Z, V=V, return_fixed_effects=True, return_covariate_coefficients=True
     )
 
-    print(f"\nTrue effect: {true_params['treatment_effect']}, Estimated effect: {results.tau:.4f}")
     assert_close(
         true_params["treatment_effect"], results.tau, tolerance, "Estimated treatment effect"
     )
 
-    print(f"Chosen lambda_L: {results.lambda_L:.4f}")
-    print(f"Chosen lambda_H: {results.lambda_H:.4f}")
-
-    print("\nFixed Effects Comparison:")
-    print("Unit Fixed Effects:")
-    print(f"True mean:      {jnp.mean(true_params['unit_fe']):.4f}")
-    print(f"Estimated mean: {jnp.mean(results.gamma):.4f}")
     assert_close(
         jnp.mean(true_params["unit_fe"]),
         jnp.mean(results.gamma),
@@ -103,9 +83,6 @@ def test_mcnnm_accuracy(tolerance=0.2):
         "Estimated unit fixed effects mean",
     )
 
-    print("\nTime Fixed Effects:")
-    print(f"True mean:      {jnp.mean(true_params['time_fe']):.4f}")
-    print(f"Estimated mean: {jnp.mean(results.delta):.4f}")
     assert_close(
         jnp.mean(true_params["time_fe"]),
         jnp.mean(results.delta),
@@ -113,10 +90,6 @@ def test_mcnnm_accuracy(tolerance=0.2):
         "Estimated time fixed effects mean",
     )
 
-    print("\nCovariate Coefficients Comparison:")
-    print("X Coefficients:")
-    print(f"True mean:      {jnp.mean(true_params['X_coef']):.4f}")
-    print(f"Estimated mean: {jnp.mean(results.H[:X.shape[1], :Z.shape[1]]):.4f}")
     assert_close(
         jnp.mean(true_params["X_coef"]),
         jnp.mean(results.H[: X.shape[1], : Z.shape[1]]),
@@ -124,9 +97,6 @@ def test_mcnnm_accuracy(tolerance=0.2):
         "Estimated X coefficients mean",
     )
 
-    print("\nZ Coefficients:")
-    print(f"True mean:      {jnp.mean(true_params['Z_coef']):.4f}")
-    print(f"Estimated mean: {jnp.mean(results.H[:X.shape[1], :Z.shape[1]].T):.4f}")
     assert_close(
         jnp.mean(true_params["Z_coef"]),
         jnp.mean(results.H[: X.shape[1], : Z.shape[1]].T),
@@ -134,9 +104,107 @@ def test_mcnnm_accuracy(tolerance=0.2):
         "Estimated Z coefficients mean",
     )
 
-    print("\nV Coefficients:")
-    print(f"True mean:      {jnp.mean(true_params['V_coef']):.4f}")
-    print(f"Estimated mean: {jnp.mean(results.beta):.4f}")
+    assert_close(
+        jnp.mean(true_params["V_coef"]),
+        jnp.mean(results.beta),
+        tolerance,
+        "Estimated V coefficients mean",
+    )
+
+
+@pytest.mark.timeout(30)
+def test_mcnnm_accuracy_no_covariates(tolerance=0.1):
+    nobs, nperiods = 100, 100
+    data, true_params = generate_data(
+        nobs=nobs,
+        nperiods=nperiods,
+        seed=42,
+        unit_fe=True,
+        time_fe=True,
+        X_cov=False,
+        Z_cov=False,
+        V_cov=False,
+    )
+
+    Y = jnp.array(data.pivot(index="unit", columns="period", values="y").values)
+    W = jnp.array(data.pivot(index="unit", columns="period", values="treat").values)
+
+    results = estimate(Y, W, return_fixed_effects=True, validation_method="holdout")
+
+    assert_close(
+        true_params["treatment_effect"], results.tau, tolerance, "Estimated treatment effect"
+    )
+
+    assert_close(
+        jnp.mean(true_params["unit_fe"]),
+        jnp.mean(results.gamma),
+        tolerance,
+        "Estimated unit fixed effects mean",
+    )
+
+    assert_close(
+        jnp.mean(true_params["time_fe"]),
+        jnp.mean(results.delta),
+        tolerance,
+        "Estimated time fixed effects mean",
+    )
+
+    assert_close(jnp.mean(true_params["L"]), jnp.mean(results.L), tolerance, "Estimated L mean")
+
+
+@pytest.mark.timeout(30)
+def test_mcnnm_accuracy(tolerance=0.2):
+    nobs, nperiods = 100, 100
+    data, true_params = generate_data(nobs=nobs, nperiods=nperiods, seed=42)
+
+    Y = jnp.array(data.pivot(index="unit", columns="period", values="y").values)
+    W = jnp.array(data.pivot(index="unit", columns="period", values="treat").values)
+
+    X, Z, V = jnp.array(true_params["X"]), jnp.array(true_params["Z"]), jnp.array(true_params["V"])
+
+    results = estimate(
+        Y,
+        W,
+        X=X,
+        Z=Z,
+        V=V,
+        return_fixed_effects=True,
+        return_covariate_coefficients=True,
+        validation_method="holdout",
+    )
+
+    assert_close(
+        true_params["treatment_effect"], results.tau, tolerance, "Estimated treatment effect"
+    )
+
+    assert_close(
+        jnp.mean(true_params["unit_fe"]),
+        jnp.mean(results.gamma),
+        tolerance,
+        "Estimated unit fixed effects mean",
+    )
+
+    assert_close(
+        jnp.mean(true_params["time_fe"]),
+        jnp.mean(results.delta),
+        tolerance,
+        "Estimated time fixed effects mean",
+    )
+
+    assert_close(
+        jnp.mean(true_params["X_coef"]),
+        jnp.mean(results.H[: X.shape[1], : Z.shape[1]]),
+        tolerance,
+        "Estimated X coefficients mean",
+    )
+
+    assert_close(
+        jnp.mean(true_params["Z_coef"]),
+        jnp.mean(results.H[: X.shape[1], : Z.shape[1]].T),
+        tolerance,
+        "Estimated Z coefficients mean",
+    )
+
     assert_close(
         jnp.mean(true_params["V_coef"]),
         jnp.mean(results.beta),
