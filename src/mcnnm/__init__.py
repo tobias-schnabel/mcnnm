d1e1744496af2ecdfa1e6f10cff1405109bceb12
Tobias Schnabel <ctobiasschnabel@gmail.com>
2024-09-01 16:24:20 +0100

    update init to expose convert_inputs convenience function to user

--- src/mcnnm/__init__.py
+++ src/mcnnm/__init__.py
@@ -1,4 +1,4 @@
 from .wrappers import estimate, MCNNMResults, complete_matrix
-from .utils import generate_data
+from .utils import generate_data, convert_inputs
 
-__all__ = ["estimate", "complete_matrix", "generate_data", "MCNNMResults"]
+__all__ = ["estimate", "complete_matrix", "generate_data", "convert_inputs", "MCNNMResults"]
