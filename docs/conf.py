e50e5de4fdc151c4b6822181af06675608f6987e
GitHub Action <action@github.com>
2025-07-05 22:12:25 +0000

    build: bump version to 1.1.2

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.path.abspath(os.path.join("..", "src")))
 project = "lightweight-mcnnm"
 copyright = "2024, Tobias Schnabel"
 author = "Tobias Schnabel"
-release = "lightweight-mcnnm 1.1.1"
+release = "1.1.2"
 
 extensions = [
     "sphinx.ext.autodoc",
